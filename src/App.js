390192d081bca837c81496ff81029c39df193a5d
Jiwook00 <jwkim775@gmail.com>
2020-08-20 23:28:54 +0900

    생활코딩 React-start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// App.js가 로드 될 때 App.css 파일 도 같이 작동 하도록 하기 위해
+// => App 이라고 하는 디자인의 컴퍼넌트를 그 앱 안에 넣는다
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Subject extends Component { //Subject 라는  Component를 만들겠다는 의미 정도
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!!
       </header>
-    </div>
-  );
+    );
+  }
 }
+// 컴포넌트를 만들 땐 하나의!! 최상위 태그만을 써야한다
+// 이것은 자바스크립트 X  -> 유사 자바스크립트
+
+class App extends Component { // react가 가지고 있는 Component 클래스를 상속시켜서  
+  render() {
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <Nav></Nav>
+      </div>
+    );
+  }
+}
+
+class Nav extends Component {
+  render() {
+    return (
+      <ul>
+        <li><a href="1.html">HTML</a></li>
+        <li><a href="2.html">HTML</a></li>
+        <li><a href="3.html">HTML</a></li>
+      </ul>
+    )
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello worl!!
+//     </div>
+//   );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; //
+//    컴포넌트      파일의 이름 여기서 app.js.를 가저온다
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+
+ReactDOM.render( //App 이 리액트를 통해 만든 사용자 정의 태그 => 즉 컴퍼넌트
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
